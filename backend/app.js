55790e5b69eb903194768beff7a3dd84f80d49d0
anthony-mini <anthony-mini@outlook.fr>
2022-10-09 19:43:33 +0200

    Envoie des controlleurs CRUD

--- backend/app.js
+++ backend/app.js
@@ -41,6 +41,10 @@ app.use((req, res, next) => {
     next();
   });
 
+  /**
+   * Enregistrer des Thing dans la base de données. 
+   */
+
   app.post('/api/stuff', (req, res, next) => {
     delete req.body._id;
     const thing = new Thing({
@@ -51,11 +55,46 @@ app.use((req, res, next) => {
       .catch(error => res.status(400).json({ error : 'Erreur rencontré lors de l\' enregistrement de l\' objet'}))
   }); 
 
-  app.use('/api/stuff', (req, res, next) => {
+  /**
+   * Récupération d'un thing spécifique'
+   */
+
+  app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+      .then(thing => res.status(200).json(thing))
+      .catch(error => res.status(404).json({ error }));
+  });
+
+  /**
+   * Récupération de la liste de Things en vente
+   */
+
+  app.get('/api/stuff', (req, res, next) => {
     Thing.find()
       .then(things => res.status(200).json(things))
       .catch(error => res.status(400).json({ error }));
   });
+
+  /**
+   * Modification d'un Thing. 
+   */
+
+  app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  /**
+   * Suppression d'une sauce 
+   */
+  
+   app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+  });  
+
 
   // Exportation de la constante `` app `` pour pouvoir y accéder dans d'autres fichiers
 module.exports = app;
